30e91e99b648850cb9e9b5cdd4ed173c293d2ce8
Hippasusss <dannyherbertgj@gmail.com>
2023-09-25 06:12:17 +0100

    cleaned up logging

--- createSymlinks.py
+++ createSymlinks.py
@@ -1,22 +1,20 @@
 import os
 from fileList import *
 
-for key, value in FilesDestination.items():
-    source = key
-    destination = os.path.join(value,key)
+for inp, output in FilesDestination.items():
+    source = inp
+    destination = os.path.join(output,inp)
     print("")
-    print("========================================================")
-    print (f"source file {source} found: {os.path.isfile(source)}")
-    print (f"desitnation file {destination} found: {os.path.isfile(destination)}")
+    print (f"source file {source} found: {os.path.isfile(source)} ------> desitnation file {destination} found: {os.path.isfile(destination)}")
 
     #check for destination path and create if not
-    if not os.path.exists(value):
+    if not os.path.exists(output):
         print("no valid destination path")
-        print(f"created at {value}")
-        os.makedirs(value)
+        print(f"created at {output}")
+        os.makedirs(output)
     # check for preexisting file and delete it if there to be replaced with link
     if os.path.isfile(destination):
-        print(f"file {destination} already exits")
+        print(f"{destination} already exits")
         if os.path.samefile(source, destination):
             print("already linked!")
             continue
@@ -25,11 +23,5 @@ for key, value in FilesDestination.items():
             os.remove(destination)
 
     #create links
-    try:
-        os.link(source, destination)
-        print (f"{source} linked to {destination}")
-    except FileExistsError:
-        print("no file")
-    except FileNotFoundError:
-        print(f"file can't be found")
-    print("========================================================")
+    os.link(source, destination)
+    print (f"{source} linked to {destination}")
