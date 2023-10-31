eaf2645b73cfe1e7e2fd59b978dd46b87a6effbf
Hippasusss <dannyherbertgj@gmail.com>
2023-10-31 06:22:59 +0000

    added bat

--- install.ps1
+++ install.ps1
@@ -1,14 +1,16 @@
 
-$installArray = @("vim.vim", "cmake", "nodejs", "python3", "Microsoft.WindowsTerminal", "Microsoft.PowerShell", "Microsoft.PowerToys", "Microsoft.VisualStudio.2022.Community", "Cockos.REAPER", "Git.Git", "JanDeDobbeleer.OhMyPosh -s winget", "fzf", "BurntSushi.ripgrep.MSVC")
+$installArray = @("vim.vim", "cmake", "nodejs", "python3", "Microsoft.WindowsTerminal", "Microsoft.PowerShell", "Microsoft.PowerToys", "Microsoft.VisualStudio.2022.Community", "Cockos.REAPER", "Git.Git", "JanDeDobbeleer.OhMyPosh -s winget", "fzf", "BurntSushi.ripgrep.MSVC", "sharkdp.bat")
 
 foreach($install in $installArray)
 {
+    Write-Output "installing: $install" 
     winget install -h --accept-package-agreements	--accept-source-agreements $install
     [console]::beep(100,100)
+    Write-Output " " 
 }
 
 Install-Module -Name CompletionPredictor -Repository PSGallery
 Import-Module -Name CompletionPredictor
 Set-PSReadLineOption -PredictionSource Plugin
 
-python3 .\createSymlinks.py
+python .\createSymlinks.py
