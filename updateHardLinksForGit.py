2b0a287c307c982e78dde74b02aa9224b1d97bb8
Hippasusss <dannyherbertgj@gmail.com>
2023-11-01 00:21:11 +0000

    fixed for symlinks

--- updateHardLinksForGit.py
+++ updateHardLinksForGit.py
@@ -1,7 +1,10 @@
-from fileList import *
+import fileList
 
-for key, value in FilesDestination.items():
+for key, value in fileList.HardLinkFilesDestination.items():
     with open(key,'r') as f:
         print(f"{f.name} has been prodded")
 
+for key, value in fileList.SoftLinkFilesDestination.items():
+    with open(key,'r') as f:
+        print(f"{f.name} has been prodded")
 
