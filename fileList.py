0249187cc086b69e14fb341dc713ae189626d6c8
Hippasusss <dannyherbertgj@gmail.com>
2024-04-11 15:10:43 +0100

    added init.vim and updated fileList with link destination

--- fileList.py
+++ fileList.py
@@ -3,16 +3,17 @@ from pathlib import Path
 
 HOME = Path.home()
 HardLinkFilesDestination = {
-    "_vimrc"                           : HOME,
-    "_vsvimrc"                         : HOME,
-    ".bashrc"                          : HOME,
-    "Tomorrow-Night.vim"               : os.path.join(HOME, "vimfiles\\colors"),
-    "coc-settings.json "               : os.path.join(HOME, "vimfiles"),
-    "coc-settings.json "               : os.path.join(HOME, "AppData\\Local\\nvim"),
-    "Microsoft.PowerShell_profile.ps1" : os.path.join(HOME, "Documents\\PowerShell"),
-    ".theme.omp.json"                  : HOME,
-    ".themeBash.omp.json"              : HOME
-    ,"settings.json"                    : os.path.join(HOME, "AppData\\Local\\Packages\\Microsoft.WindowsTerminal_8wekyb3d8bbwe\\LocalState")
+    "_vimrc":                           HOME,
+    "_vsvimrc":                         HOME,
+    ".bashrc":                          HOME,
+    "Tomorrow-Night.vim":               os.path.join(HOME, "vimfiles\\colors"),
+    "coc-settings.json ":               os.path.join(HOME, "vimfiles"),
+    "coc-settings.json ":               os.path.join(HOME, "AppData\\Local\\nvim"),
+    "Microsoft.PowerShell_profile.ps1": os.path.join(HOME, "Documents\\PowerShell"),
+    ".theme.omp.json":                  HOME,
+    ".themeBash.omp.json":              HOME,
+    "init.vim":                         os.path.join(HOME, 'AppData\\Local\\nvim'),
+    "settings.json":                    os.path.join(HOME, "AppData\\Local\\Packages\\Microsoft.WindowsTerminal_8wekyb3d8bbwe\\LocalState")
 }
 
 SoftLinkFilesDestination = {
