4308e3c0946364fd1ada10cd30b1c60f72af2329
Hippasusss <dannyherbertgj@gmail.com>
2025-03-27 01:53:38 +0000

    removed unnecesary stuff

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 
 #WINGET
-$installArray = @("vim.vim", "Neovim.Neovim", "cmake", "nodejs", "python3", "Microsoft.WindowsTerminal", "Microsoft.PowerShell",  "Microsoft.VisualStudio.2022.Community", "Cockos.REAPER", "Git.Git", "JanDeDobbeleer.OhMyPosh", "fzf", "BurntSushi.ripgrep.MSVC", "sharkdp.bat")
+$installArray = @("Neovim.Neovim", "cmake", "nodejs", "python3", "Microsoft.WindowsTerminal", "Microsoft.PowerShell",  "Microsoft.VisualStudio.2022.Community", "Cockos.REAPER", "Git.Git", "JanDeDobbeleer.OhMyPosh", "fzf", "BurntSushi.ripgrep.MSVC", "sharkdp.bat", "sharkdp.fd")
 
 foreach($install in $installArray)
 {
@@ -9,16 +9,6 @@ foreach($install in $installArray)
     [console]::beep(100,100)
     Write-Output " " 
 }
-
-#NVIM PLUG
-Write-Output "installing: Nvim Plug" 
-iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
-    ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
-
-#VIM PLUG
-Write-Output "installing: vim Plug" 
-iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
-    ni $HOME/vimfiles/autoload/plug.vim -Force
 
 #VIM BACKUP FILES
 $vimBackupPath = "~\\.config\back"
@@ -37,16 +27,6 @@ Write-Output "Installing Powershell CompletionPredictor"
 Install-Module -Name CompletionPredictor -Repository PSGallery -Force
 Import-Module -Name CompletionPredictor
 Set-PSReadLineOption -PredictionSource Plugin
-
-#FONT
-Write-Output "Installing Fonts" 
-$FontList = Get-ChildItem -Path ".\\Meslo\*" -Include ('*.fon','*.otf','*.ttc','*.ttf')
-
-foreach ($Font in $FontList) {
-    Write-Host 'Installing font -' $Font.BaseName
-    Copy-Item $Font "C:\Windows\Fonts" -ErrorAction SilentlyContinue
-    New-ItemProperty -Name $Font.BaseName -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -PropertyType string -Value $Font.name -ErrorAction SilentlyContinue       
-}
 
 #LINKS
 Write-Output "Creating Symlinks" 
