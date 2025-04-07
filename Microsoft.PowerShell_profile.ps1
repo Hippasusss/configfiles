f1481236d8fae4674f97ab3d12a04625fa210896
Hippasusss <dannyherbertgj@gmail.com>
2025-04-07 01:52:12 +0100

    Added autocompete options for powershell

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,3 +5,8 @@ Import-Module -Name CompletionPredictor
 Set-PSReadLineOption -PredictionSource Plugin 
 Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Vi
+
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadlineKeyHandler -Key Tab -Function NextSuggestion 
+Set-PSReadlineKeyHandler -Key Shift+Tab -Function PreviousSuggestion 
