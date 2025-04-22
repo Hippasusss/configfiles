87f484833f03f93427de66ba0d49ae03480ed333
Hippasusss <dannyherbertgj@gmail.com>
2025-04-22 23:35:47 +0100

    removed coc-settings

--- fileList.py
+++ fileList.py
@@ -7,8 +7,6 @@ HardLinkFilesDestination = {
     "_vsvimrc":                         HOME,
     ".bashrc":                          HOME,
     "Tomorrow-Night.vim":               os.path.join(HOME, "vimfiles\\colors"),
-    "coc-settings.json ":               os.path.join(HOME, "vimfiles"),
-    "coc-settings.json ":               os.path.join(HOME, "AppData\\Local\\nvim"),
     "Microsoft.PowerShell_profile.ps1": os.path.join(HOME, "Documents\\PowerShell"),
     ".theme.omp.json":                  HOME,
     ".themeBash.omp.json":              HOME,
