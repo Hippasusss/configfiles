3b8e9d5b45f6dd015c8a2102854020fbfbe6870b
Hippasusss <dannyherbertgj@gmail.com>
2026-03-31 00:17:43 +0100

    serach home folder

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -59,3 +59,26 @@ function fff {
         }
     }
 }
+function ffh {
+    param(
+        [string]$Path = "~",
+        [switch]$sl
+    )
+    $basePath = Resolve-Path $Path
+    $selection = fd -t f -t d -H --base-directory $basePath | fzf
+    if ($selection) {
+        $fullPath = Join-Path $basePath $selection
+        if (Test-Path $fullPath -PathType Container) {
+            if ($sl) {
+                Set-Location $fullPath
+            }
+        } else {
+            if ($sl) {
+                Set-Location (Split-Path $fullPath -Parent)
+                nvim (Split-Path $fullPath -Leaf)
+            } else {
+                nvim $fullPath
+            }
+        }
+    }
+}
