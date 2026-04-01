9117c6f1fbe9ac9706135737fb6e047cf090a478
Hippasusss <dannyherbertgj@gmail.com>
2026-04-01 03:58:44 +0100

    removed unused and simplified as much as possible

--- init.lua
+++ init.lua
@@ -1,10 +1,9 @@
 vim.opt.runtimepath:prepend("~/vimfiles")
 vim.opt.runtimepath:append("~/vimfiles/after")
-vim.g.mapleader, vim.g.mapleaderlocal = ",", ","
+vim.g.mapleader, vim.g.maplocalleader = ",", ","
 
-vim.pack.add ({
+vim.pack.add({
     "https://github.com/rebelot/kanagawa.nvim",
-    "https://github.com/nvim-tree/nvim-web-devicons",
     "https://github.com/ibhagwan/fzf-lua",
     "https://github.com/stevearc/oil.nvim",
     "https://github.com/neovim/nvim-lspconfig",
@@ -17,58 +16,50 @@ vim.pack.add ({
     "https://github.com/folke/snacks.nvim",
     "https://github.com/gennaro-tedesco/nvim-possession",
     "https://github.com/nvim-treesitter/nvim-treesitter",
-    "https://github.com/kylechui/nvim-surround",
-    "https://github.com/Hippasusss/easypeasy",
-    "https://github.com/Hippasusss/diyank",
     { src = 'https://github.com/Saghen/blink.cmp', version = vim.version.range('*') },
 })
+vim.opt.rtp:prepend("~/Projects/nvim/easypeasy")
+vim.opt.rtp:prepend("~/Projects/nvim/diyank")
 
-for _, p in ipairs({"oil", "roslyn", "nvim-surround", "diyank", "easypeasy"}) do require(p).setup() end
+for _, p in ipairs({"oil", "roslyn", "diyank", "easypeasy"}) do require(p).setup() end
 
 vim.cmd.colorscheme("kanagawa")
 vim.api.nvim_set_hl(0, "SignColumn", { bg = "none", ctermbg = "none" })
 vim.api.nvim_set_hl(0, "LineNr", { bg = "none", ctermbg = "none" })
 
-local fzf = require("fzf-lua")
-fzf.setup({
+require("fzf-lua").setup({
     file_ignore_patterns = { vim.fn.expand("config/back/") },
-    files = { cmd = 'rg --files --follow --smart-case --color=never --glob !.git --glob !build' }
+    files = { cmd = 'rg --files --follow --smart-case --color=never --glob !.git --glob !build' },
+    ui_select = true;
 })
-fzf.register_ui_select()
 
 require("blink.cmp").setup({
-    keymap = {
-        preset = "none",
+    keymap = { preset = "none",
         ['<tab>'] = {'snippet_forward', 'select_next', 'fallback'},
         ['<s-tab>'] = {'snippet_backward', 'select_prev', 'fallback'},
         ['<enter>'] = {'accept', 'fallback'},
     },
-    sources = { default = { 'lsp', 'snippets'} },
+    sources = { default = { 'lsp', 'snippets', 'codecompanion'} },
     cmdline = { completion = { menu = { auto_show = true }, list = { selection = { preselect = false , auto_insert = true}} } },
     completion = { list = { selection = { auto_insert = true, preselect = false } } , documentation = {auto_show = true} },
     signature = { enabled = true },
 })
 
 require("mason").setup({ registries = { "github:mason-org/mason-registry", "github:crashdummyy/mason-registry", }, })
-vim.lsp.enable({'lua_ls', 'clangd', 'html' , 'cssls', 'roslyn', 'neocmake', 'powershell_es'})
 vim.lsp.config('powershell_es', { bundle_path = vim.fn.stdpath('data') .. '/mason/packages/powershell-editor-services', })
 vim.lsp.config('lua_ls', { settings = { Lua = { workspace = { library = vim.api.nvim_get_runtime_file("", true) }, } }})
+vim.lsp.enable({'lua_ls', 'clangd', 'html' , 'cssls', 'roslyn', 'neocmake', 'powershell_es'})
 
-local function loadapikey(key)
-    return assert(vim.json.decode(table.concat(vim.fn.readfile(vim.fn.expand("~/.secret/keys.json")), "\n"))[key], "missing key: "..key)
-end
+local function loadapikey(key) return assert(vim.json.decode(table.concat(vim.fn.readfile(vim.fn.expand("~/.secret/keys.json")), "\n"))[key], "missing key: "..key) end
 require("codecompanion").setup({
-    strategies = { chat = { adapter = "deepseek" }, inline = { adapter = "deepseek" }, cmd = { adapter = "deepseek" } },
-    adapters = {
-        http = {
-            deepseek = function()
-                return require("codecompanion.adapters").extend("deepseek", {
-                    env = { api_key = loadapikey("deepseek_api_key") },
-                    schema = { model = { default = "deepseek-chat" } },
+    interactions = { chat = { adapter = "deepseek" }, inline = { adapter = "deepseek" }, cmd = { adapter = "deepseek" } },
+    adapters = { http = {
+        deepseek = function()
+            return require("codecompanion.adapters").extend("deepseek", {
+                env = { api_key = loadapikey("deepseek_api_key") },
+                schema = { model = { default = "deepseek-chat" } },
                 })
-            end,
-        },
-    },
+        end, opts = { show_presets = false }, }},
 })
 
 local has_poss, poss = pcall(require, 'nvim-possession')
@@ -89,12 +80,8 @@ require("snacks").setup({ bigfile = { enabled = true }, lazygit = { enabled = tr
 local session_path = vim.fn.expand("~/vimfiles/session/")
 vim.fn.mkdir(session_path, "p")
 require("nvim-possession").setup({
-    sessions = {
-        sessions_path = session_path,
-        sessions_icon = ''
-    },
-    autoload = true,
-    autosave = true,
+    sessions = { sessions_path = session_path, sessions_icon = '' },
+    autoload = true, autosave = true,
     save_hook = function()
         for _, buf in ipairs(vim.api.nvim_list_bufs()) do
             if vim.api.nvim_buf_get_name(buf):match("CodeCompanion") then
@@ -106,29 +93,24 @@ require("nvim-possession").setup({
 
 require("nvim-treesitter").setup({
     ensure_installed = { "c", "c_sharp", "css", "lua", "vim", "vimdoc", "cpp", "python", "html" },
-    highlight = { enable = true, additional_vim_regex_highlighting = false, },
+    highlight = { enable = true, },
     indent = { enable = true },
 })
 
 --AutoCommands
-local autocmd = vim.api.nvim_create_autocmd
-autocmd('PackChanged', {
-    callback = function(e) if e.data.kind == 'update' and e.data.spec.name == 'nvim-treesitter' then pcall(function() vim.cmd('TSUpdate') end) end end
-})
-autocmd('BufWinEnter', { desc = 'Return cursor to last position', command = 'silent! normal! g`"zv' })
-autocmd('TextYankPost', { desc = 'Highlight yanked text', callback = function() vim.hl.on_yank({higroup = 'Substitute', timeout = 200}) end })
+vim.api.nvim_create_autocmd('PackChanged', { callback = function(e) if e.data.kind == 'update' and e.data.spec.name == 'nvim-treesitter' then pcall(function() vim.cmd('TSUpdate') end) end end })
+vim.api.nvim_create_autocmd('BufWinEnter', { desc = 'Return cursor to last position', command = 'silent! normal! g`"zv' })
+vim.api.nvim_create_autocmd('TextYankPost', { desc = 'Highlight yanked text', callback = function() vim.hl.on_yank({higroup = 'Substitute', timeout = 200}) end })
 
 
 local backuppath = vim.fn.expand("~\\vimfiles\\back")
 vim.fn.mkdir(backuppath, "p")
 
 --Options
-vim.g.completion_enable_auto_popup = 0
 vim.diagnostic.config({ virtual_lines = { current_line = true }, })
 vim.opt.undodir, vim.opt.backupdir, vim.opt.directory = backuppath, backuppath, backuppath
 vim.opt.shell = 'powershell.exe'
 vim.opt.undofile, vim.opt.swapfile, vim.opt.backup = true, false, true
-vim.opt.undolevels = 1000
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.signcolumn = "yes"
 vim.opt.tabstop, vim.opt.shiftwidth = 4, 4
@@ -138,9 +120,7 @@ vim.opt.wrap, vim.opt.linebreak = true, true
 vim.opt.ignorecase, vim.opt.smartcase = true, true
 vim.opt.splitbelow, vim.opt.splitright = true, true
 vim.opt.visualbell, vim.opt.wildmenu, vim.opt.cursorline = true, true, true
-vim.opt.encoding = "utf-8"
 vim.opt.incsearch, vim.opt.hlsearch = true, false
-vim.opt.paste = false
 vim.opt.sessionoptions = "blank,curdir,folds,help,tabpages,winsize,terminal"
 
 -- Mappings
